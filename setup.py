b8412d6402b527ef8c0443c944734d8857107748
Eniatorudabo Abomaye <104331036+yickysan@users.noreply.github.com>
2023-09-17 19:24:46 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
 
 __version__ = "0.0.0"
-REPO_NAME = "nairobi-kenya-road-accident"
+REPO_NAME = "omdena-kenya-task-5-web-development"
 ORGANIZATION = "OmdenaAI"
 AUTHOR_NAME = "Abomaye Victor"
 SRC_NAME = "OmdenaKenyaRoadAccidents"
@@ -17,9 +17,9 @@ setuptools.setup(
     description = "A python package for predicting road accidents severity in Kenya",
     long_description = long_description,
     long_description_content = "text/markdown",
-    url = f"https://www.github.com/{ORGANIZATION}/{REPO_NAME}",
+    url = f"https://www.github.com/yickysan/{REPO_NAME}",
     project_urls = {
-        "Bug Tracker": f"https://www.github.com/{ORGANIZATION}/{REPO_NAME}/issues"
+        "Bug Tracker": f"https://www.github.com/yickysan/{REPO_NAME}/issues"
     },
     package_dir = {"": "src"},
     packages = setuptools.find_packages(where = "src")
